f529f84e653621a7629b1de20a469b5bc28bcccd
Anagha-lin <135278326+Anagha-lin@users.noreply.github.com>
2023-07-26 04:19:24 +0930

    Update functions.c

--- functions.c
+++ functions.c
@@ -6,7 +6,7 @@
  * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculating active flags
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
