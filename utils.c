8ed63372359936f5e4dc502eff539ca956188948
Anagha-lin <135278326+Anagha-lin@users.noreply.github.com>
2023-07-26 04:32:53 +0930

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  * is_printable - Evaluates if a char is printable
- *   * @c: Chars to be evaluated.
+ *  * is_printable - Evaluates if a chars
+ *   * @c: Char to be evaluated.
  *    *
  *     * Return: 1 if c is printable, 0 otherwise
  *      */
